e0efa39230b53dfe8f995057ab88d7eefed7379a
miya0001 <miya@wpist.me>
2017-02-06 05:22:36 +0900

    update release name

--- miya-gallery.php
+++ miya-gallery.php
@@ -5,7 +5,7 @@
  * Description:     Grid style gallery plugin.
  * Author:          Takayuki Miyauchi
  * Author URI:      https://miya.io/
- * Version:         0.1.9
+ * Version:         0.1.10
  *
  * @package         Miya_Gallery
  */
