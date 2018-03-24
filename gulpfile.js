d04216e4c2d7920d4d8ed4ef3448c8c5a162e015
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-24 21:36:08 +0900

    improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
-var gulp = require('gulp'),
-	concat = require('gulp-concat'),
-	minifyCSS = require('gulp-minify-css'),
-	uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var csso = require('gulp-csso');
 
 gulp.task('js', function(){
 	return gulp.src(["node_modules/simplelightbox/dist/simple-lightbox.js", "src/*.js"])
 		.pipe(concat('script.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('js'));
+		.pipe(gulp.dest('./js'));
+});
+
+gulp.task('sass', function () {
+	return gulp.src('./src/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./src'));
 });
 
 gulp.task('css', function(){
 	return gulp.src(["node_modules/simplelightbox/dist/simplelightbox.css", "src/*.css"])
 		.pipe(concat('style.min.css'))
 		.pipe(minifyCSS())
-		.pipe(gulp.dest('css'))
+		.pipe(csso())
+		.pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', ['js', 'css']);
+gulp.task('default', ['js', 'sass', 'css']);
